e5333fca501942620fcef1870fbf14650ec1b406
Jannik <Jannik.lading@hotmail.dk>
2019-01-30 11:24:06 +0100

    Upload calculator

---
+++ Calculator.Test.Unit/Testunit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; 
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class TestUnit
+    {
+
+        [Test]
+        public void Add_Add2and2_Return4()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Add(2,2), Is.EqualTo(4));
+        }
+    }
+}
+
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b; 
+        }
+        double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        double Power(double a, double exp)
+        {
+            return Math.Pow(a,exp);
+        }
+
+    }
+}
