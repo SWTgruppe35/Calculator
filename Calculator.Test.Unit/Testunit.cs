61bd76b7742d112f9960573d95782970f70bf69f
Nichlaes <nichlaes.soerensen@gmail.com>
2019-01-30 11:51:22 +0100

    Added testcases for Add()

--- Calculator.Test.Unit/Testunit.cs
+++ Calculator.Test.Unit/Testunit.cs
@@ -10,12 +10,29 @@ namespace Calculator.Test.Unit
     [TestFixture]
     public class TestUnit
     {
+        private Calculator _uut;
+
+        // Using the SetUp feature, saves coding (part of) the Arrange step
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Calculator();
+        }
 
         [Test]
         public void Add_Add2and2_Return4()
         {
-            var uut = new Calculator();
-            Assert.That(uut.Add(2,2), Is.EqualTo(4));
+            Assert.That(_uut.Add(2,2), Is.EqualTo(4));
+        }
+        [TestCase(3, 2, 5)]
+        [TestCase(-3, -2, -5)]
+        [TestCase(-3, 2, -1)]
+        [TestCase(3, -2, 1)]
+        [TestCase(3, 0, 3)]
+        public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
+        {
+            // Combining the Act and Assert steps is also possible
+            Assert.That(_uut.Add(a, b), Is.EqualTo(result));
         }
     }
 }
