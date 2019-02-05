9c4dc2637d9043c84557716dfe4297799e0702cd
Nichlaes <nichlaes.soerensen@gmail.com>
2019-02-05 16:19:44 +0100

    Added testcase for multiply, subtract and divide

--- Calculator.Test.Unit/Testunit.cs
+++ Calculator.Test.Unit/Testunit.cs
@@ -10,7 +10,8 @@ namespace Calculator.Test.Unit
     [TestFixture]
     public class TestUnit
     {
-        private Calculator _uut;
+
+        private Calculator _uut = new Calculator();
 
         // Using the SetUp feature, saves coding (part of) the Arrange step
         [SetUp]
@@ -19,11 +20,7 @@ namespace Calculator.Test.Unit
             _uut = new Calculator();
         }
 
-        [Test]
-        public void Add_Add2and2_Return4()
-        {
-            Assert.That(_uut.Add(2,2), Is.EqualTo(4));
-        }
+
         [TestCase(3, 2, 5)]
         [TestCase(-3, -2, -5)]
         [TestCase(-3, 2, -1)]
@@ -34,6 +31,58 @@ namespace Calculator.Test.Unit
             // Combining the Act and Assert steps is also possible
             Assert.That(_uut.Add(a, b), Is.EqualTo(result));
         }
-    }
+
+        [TestCase(3, 2, 1)]
+        [TestCase(-3, -2, -1)]
+        [TestCase(-3, 2, -5)]
+        [TestCase(0, 4, -4)]
+        [TestCase(3, 0, 3)]
+        public void Sub_SubPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(_uut.Subtract(a,b), Is.EqualTo(result));
+            
+        }
+
+        [TestCase(3, 2, 6)]
+        [TestCase(-3, -2, 6)]
+        [TestCase(-3, 2, -6)]
+        [TestCase(0, 4, 0)]
+        [TestCase(3, 1, 3)]
+        public void Multiply_PosAndNegNum_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(_uut.Multiply(a,b), Is.EqualTo(result));
+
+        }
+
+        [TestCase(6, 6, 1)]
+        [TestCase(-6, -3, 2)]
+        [TestCase(10, 2, 5)]
+        [TestCase(4, 1, 4)]
+        [TestCase(0, 1, 0)]
+        public void Divide_PosAndNegNum_ResultIsCorrect(int a, int b, int result)
+        {
+            Assert.That(_uut.Divide(a, b), Is.EqualTo(result));
+
+        }
+
+        [Test]
+        public void Divide_withZero_ExpThrown()
+        {
+            var thrown = false;
+
+            try
+            {
+                _uut.Divide(10,0);
+            }
+            catch (DivideByZeroException)
+            {
+                thrown = true;
+            }
+
+            Assert.True(thrown);
+        }
+
+        
+    };
 }
 
